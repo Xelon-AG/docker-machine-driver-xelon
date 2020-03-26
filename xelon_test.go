129ce5373ebb7f8948e4d233dc1135bc35dfa5e2
pavel <36902936+pavel-github@users.noreply.github.com>
2020-03-26 01:07:35 +0100

    Remove obsolete tests for username and password options check

--- xelon_test.go
+++ xelon_test.go
@@ -7,26 +7,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDriver_PreCreateCheck_MissingPassword(t *testing.T) {
+func TestDriver_PreCreateCheck_MissingToken(t *testing.T) {
 	driver := NewDriver("default", "path")
 	flags := &drivers.CheckDriverOptions{
 		FlagsValues: map[string]interface{}{
-			"xelon-password": "",
-			"xelon-username": "user@xelon.ch",
-		},
-		CreateFlags: driver.GetCreateFlags(),
-	}
-
-	err := driver.SetConfigFromFlags(flags)
-	assert.Error(t, err)
-}
-
-func TestDriver_PreCreateCheck_MissingUsername(t *testing.T) {
-	driver := NewDriver("default", "path")
-	flags := &drivers.CheckDriverOptions{
-		FlagsValues: map[string]interface{}{
-			"xelon-password": "password",
-			"xelon-username": "",
+			"xelon-token": "",
 		},
 		CreateFlags: driver.GetCreateFlags(),
 	}
@@ -40,8 +25,7 @@ func TestDriver_PreCreateCheck_DevicePasswordLessThen6Characters(t *testing.T) {
 	flags := &drivers.CheckDriverOptions{
 		FlagsValues: map[string]interface{}{
 			"xelon-device-password": "12345",
-			"xelon-password": "password",
-			"xelon-username": "user@xelon.ch",
+			"xelon-token": "token",
 		},
 		CreateFlags: driver.GetCreateFlags(),
 	}
